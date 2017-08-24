cd17043a25766192af5b2c4f5c87e17bdb121dee
NicolasGay <gayn@hotmail.fr>
2017-08-24 13:23:04 +0200

    premier commit teamBG

--- src/main/java/io/robusta/auth/domain/User.java
+++ src/main/java/io/robusta/auth/domain/User.java
@@ -1,8 +1,50 @@
 package io.robusta.auth.domain;
 
-public interface User {
 
-	String getEmail();
+public class User implements UserInterface{
 
+    private String name;
+    private String email;
+    private String password;
 
+    public User() {
+    }
+
+    public User(String username, String email, String password) {
+        this.name = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/io/robusta/auth/domain/UserInterface.java
@@ -0,0 +1,10 @@
+package io.robusta.auth.domain;
+
+public interface UserInterface {
+
+	String getEmail();
+	String getPassword();
+	String getName();
+
+
+}
